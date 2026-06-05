3afa198d838e67de5722d310b85e84e8184e205c
Yonas Mindaye <yonasmindaye04@gmail.com>
2026-06-05 22:32:04 +0300

    eslint added

---
+++ backend/eslint.config.js
@@ -0,0 +1,12 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
